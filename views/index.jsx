6a0668ba161b11e3ebcafc56025e62402de7a1d4
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-04-19 20:00:53 -0700

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -4,6 +4,12 @@ const Default = require('./layouts/Default')
 function Index ({breads})  {
   return (
 <Default>
+<div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+<div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
   <h2>Index Page</h2>
   {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
