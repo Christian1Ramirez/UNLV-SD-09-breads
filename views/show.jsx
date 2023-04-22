23fb7a09bc2278f0f631eac82dbb35ce5ec45abc
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-04-22 11:45:57 -0700

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -16,11 +16,12 @@ function Show ({bread, index}) {
     }
     have gluten.
   </p>
-   <form action={`/breads/${index}?_method=DELETE`} method="POST">
-    <input type='submit' value="DELETE"/>
-   </form>
   <img src={bread.image} alt={bread.name} />
   <li><a href="/breads">Go home</a></li>
+   <form action={`/breads/${index}?_method=DELETE`} method="POST">
+    <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+    <input type='submit' value="DELETE"/>
+   </form>
 </Default>
    )
 }
